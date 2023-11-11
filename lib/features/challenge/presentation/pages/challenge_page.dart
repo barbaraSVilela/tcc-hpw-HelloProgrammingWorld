8c80f77763eded4ccb2fb60f447e5dec8603482f
HPW <hpw.application@gmail.com>
2023-11-11 19:03:43 -0300

    Finished UI in challenge page

--- lib/features/challenge/presentation/pages/challenge_page.dart
+++ lib/features/challenge/presentation/pages/challenge_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:tcc_hpw_hello_programming_world/config/themes/app_theme.dart';
-import 'package:tcc_hpw_hello_programming_world/features/challenge/domain/entity/challenge.dart';
 import 'package:tcc_hpw_hello_programming_world/features/challenge/presentation/bloc/challenge_bloc.dart';
 
 class ChallengePage extends StatefulWidget {
@@ -14,7 +13,6 @@ class ChallengePage extends StatefulWidget {
 
 class _ChallengePageState extends State<ChallengePage> {
   late ChallengeBloc _bloc;
-  final List<String> _selectedOptions = [];
   @override
   void initState() {
     _bloc = GetIt.I<ChallengeBloc>();
@@ -32,7 +30,6 @@ class _ChallengePageState extends State<ChallengePage> {
         if (state is Loading) {
           return const _LoadingChallenge();
         } else if (state is Loaded) {
-          Challenge challenge = state.challenge;
           // return Column(
           //   children: [
           //     Expanded(
@@ -50,54 +47,107 @@ class _ChallengePageState extends State<ChallengePage> {
           return SafeArea(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          "Nível ${state.challenge.level}",
-                          style: AppTheme.themeData.textTheme.titleSmall!
-                              .copyWith(color: AppTheme.colorScheme.secondary),
-                        ),
-                        Text(
-                          "Tentativa 1",
-                          style: AppTheme.themeData.textTheme.titleSmall!
-                              .copyWith(color: AppTheme.colorScheme.secondary),
-                        ),
-                      ],
+              child: LayoutBuilder(
+                builder: (context, constraints) => SingleChildScrollView(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: constraints.maxHeight,
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: AppTheme.colorScheme.primary,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          state.challenge.prompt,
-                          textAlign: TextAlign.center,
-                          style:
-                              AppTheme.themeData.textTheme.titleLarge!.copyWith(
-                            color: AppTheme.colorScheme.primaryContainer,
+                    child: IntrinsicHeight(
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 5),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Nível ${state.challenge.level}",
+                                style: AppTheme.themeData.textTheme.titleSmall!
+                                    .copyWith(
+                                        color: AppTheme.colorScheme.secondary),
+                              ),
+                              Text(
+                                "Tentativa 1",
+                                style: AppTheme.themeData.textTheme.titleSmall!
+                                    .copyWith(
+                                        color: AppTheme.colorScheme.secondary),
+                              ),
+                            ],
                           ),
-                        ),
+                          const SizedBox(height: 5),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: AppTheme.colorScheme.primary,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Text(
+                                state.challenge.prompt,
+                                textAlign: TextAlign.center,
+                                style: AppTheme.themeData.textTheme.titleLarge!
+                                    .copyWith(
+                                  color: AppTheme.colorScheme.primaryContainer,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Wrap(
+                            spacing: 10,
+                            runSpacing: 10,
+                            direction: Axis.horizontal,
+                            children: List.generate(
+                              state.challenge.options.length,
+                              (index) => _PillContainer(
+                                text: state.challenge.options[index],
+                                backgroundColor:
+                                    AppTheme.colorScheme.primaryContainer,
+                                textColor: Colors.black,
+                              ),
+                            ),
+                            // children: [_PillContainer(text: 'test')],
+                          ),
+                          const Spacer(),
+                          const SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              _PillContainer(
+                                text: 'Desistir',
+                                backgroundColor: AppTheme.colorScheme.secondary,
+                                textColor: Colors.white,
+                              ),
+                              _PillContainer(
+                                text: 'Submeter',
+                                backgroundColor: AppTheme.colorScheme.primary,
+                                textColor: Colors.white,
+                              )
+                            ],
+                          ),
+                          const Divider(thickness: 2),
+                          const SizedBox(height: 20),
+                          Wrap(
+                            spacing: 10,
+                            runSpacing: 10,
+                            direction: Axis.horizontal,
+                            children: List.generate(
+                              state.challenge.options.length,
+                              (index) => _PillContainer(
+                                text: state.challenge.options[index],
+                                backgroundColor:
+                                    AppTheme.colorScheme.primaryContainer,
+                                textColor: Colors.black,
+                              ),
+                            ),
+                            // children: [_PillContainer(text: 'test')],
+                          ),
+                          const Spacer(),
+                        ],
                       ),
                     ),
-                    Wrap(
-                      direction: Axis.horizontal,
-                      children: List.generate(
-                        state.challenge.options.length,
-                        (index) => _PillContainer(
-                          text: state.challenge.options[index],
-                        ),
-                      ),
-                      // children: [_PillContainer(text: 'test')],
-                    ),
-                    const Divider(thickness: 2),
-                  ],
+                  ),
                 ),
               ),
             ),
@@ -108,139 +158,36 @@ class _ChallengePageState extends State<ChallengePage> {
       },
     ));
   }
-
-  Widget _displayChallengePrompt(BuildContext context, String challengePrompt) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 40),
-        child: Card(
-            elevation: 10,
-            color: AppTheme.colorScheme.primary,
-            shape: RoundedRectangleBorder(
-                side: BorderSide(color: AppTheme.colorScheme.outline),
-                borderRadius: const BorderRadius.all(Radius.circular(8))),
-            child: Container(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    Text("DESAFIO",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: AppTheme.colorScheme.onPrimary)),
-                    SingleChildScrollView(
-                      physics: const ClampingScrollPhysics(),
-                      child: Text(challengePrompt,
-                          style: TextStyle(
-                              color: AppTheme.colorScheme.onPrimary,
-                              fontSize: 16),
-                          textAlign: TextAlign.justify),
-                    )
-                  ],
-                ))));
-  }
-
-  Widget _displayChallenge(BuildContext context, List<String> solution) {
-    return GridView.count(
-      crossAxisCount: 8,
-      padding: const EdgeInsets.all(8.0),
-      children: _transformAttempt(),
-    );
-  }
-
-  void _onItemTappedRemove(String item) {
-    setState(() {
-      _selectedOptions.remove(item);
-    });
-  }
-
-  Widget _attempt(String data) {
-    return GestureDetector(
-        onTap: () {
-          _onItemTappedRemove(data);
-        },
-        child: Container(
-          color: AppTheme.colorScheme.secondary,
-          padding: const EdgeInsets.all(10),
-          child: Text(data,
-              style: TextStyle(color: AppTheme.colorScheme.onSecondary)),
-        ));
-  }
-
-  List<Widget> _transformAttempt() {
-    List<Widget> attempts = [];
-    attempts = _selectedOptions.map((String e) => _attempt(e)).toList();
-    return attempts;
-  }
-
-  Widget _displayButtonsAndInfo(BuildContext context, int level, int attempt) {
-    return Container(
-      alignment: Alignment.topCenter,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          const ElevatedButton(onPressed: null, child: Text("Run")),
-          Text("Nível: $level"),
-          Text("Tentativa: $attempt"),
-          const ElevatedButton(onPressed: null, child: Text("Desistir"))
-        ],
-      ),
-    );
-  }
-
-  Widget _buildChallenge(
-      BuildContext context, List<String> solution, List<String> options) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          _displayChallenge(context, solution),
-          _displayOptions(context, options)
-        ],
-      ),
-    );
-  }
-
-  Widget _displayOptions(BuildContext context, List<String> options) {
-    return ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: options.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            child: Container(
-                color: AppTheme.colorScheme.tertiary,
-                child: Text(options[index],
-                    style: TextStyle(color: AppTheme.colorScheme.onTertiary))),
-            onTap: () {
-              _onTappedAdd(options[index]);
-            },
-          );
-        });
-  }
-
-  void _onTappedAdd(String data) {
-    setState(() {
-      _selectedOptions.add(data);
-    });
-  }
 }
 
 class _PillContainer extends StatelessWidget {
-  const _PillContainer({required this.text});
+  const _PillContainer({
+    required this.text,
+    this.onTap,
+    required this.backgroundColor,
+    required this.textColor,
+  });
 
   final String text;
+  final VoidCallback? onTap;
+  final Color backgroundColor;
+  final Color textColor;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: AppTheme.colorScheme.primaryContainer,
-      ),
-      child: Text(
-        text,
-        style: AppTheme.themeData.textTheme.labelLarge,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: backgroundColor,
+        ),
+        child: Text(
+          text,
+          style: AppTheme.themeData.textTheme.labelLarge!
+              .copyWith(color: textColor),
+        ),
       ),
     );
   }
