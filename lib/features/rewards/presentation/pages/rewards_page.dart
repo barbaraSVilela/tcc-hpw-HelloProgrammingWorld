e68c4a88a9f0d59dc1751b04d7d7a71d703039bd
HPW <hpw.application@gmail.com>
2023-11-15 23:53:29 -0300

    Add padding to rewards page

--- lib/features/rewards/presentation/pages/rewards_page.dart
+++ lib/features/rewards/presentation/pages/rewards_page.dart
@@ -4,7 +4,8 @@ import 'package:get_it/get_it.dart';
 import 'package:tcc_hpw_hello_programming_world/config/themes/app_theme.dart';
 import 'package:tcc_hpw_hello_programming_world/features/rewards/domain/entities/reward.dart';
 import 'package:tcc_hpw_hello_programming_world/features/rewards/presentation/bloc/reward_bloc.dart';
-import 'package:tcc_hpw_hello_programming_world/features/user/presentation/bloc/user_bloc.dart' as userBloc;
+import 'package:tcc_hpw_hello_programming_world/features/user/presentation/bloc/user_bloc.dart'
+    as userBloc;
 
 class RewardsPage extends StatefulWidget {
   const RewardsPage({super.key});
@@ -31,7 +32,7 @@ class _RewardsPageState extends State<RewardsPage> {
         bloc: _bloc,
         builder: (context, state) {
           if (state is Loading) {
-            return Scaffold(body:Center(child: _Loading()));
+            return Scaffold(body: Center(child: _Loading()));
           } else if (state is Loaded) {
             return Scaffold(
                 appBar: AppBar(
@@ -40,28 +41,42 @@ class _RewardsPageState extends State<RewardsPage> {
                   centerTitle: true,
                   title: Text("LOJA"),
                   leading: BackButton(onPressed: () => Navigator.pop(context)),
-                  actions: [FittedBox(
-                    fit: BoxFit.scaleDown,
-                    child: _userCoins(context),
-                  ),const Icon(Icons.add_business_outlined)],
+                  actions: [
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: _userCoins(context),
+                    ),
+                    const Icon(Icons.add_business_outlined)
+                  ],
                 ),
                 body: ListView.builder(
                     itemCount: state.rewards.length,
                     itemBuilder: (context, index) {
                       Reward currentReward = state.rewards[index];
-                      return ListTile(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3),side: BorderSide(color: Colors.black)),
-                          tileColor: AppTheme.colorScheme.primaryContainer,
-                          contentPadding: EdgeInsets.all(10) ,
-                          leading: Image.network(currentReward.image ),
-                          title: Text(currentReward.title,style:  AppTheme.themeData.textTheme.titleSmall!.copyWith(
-                              color: AppTheme.colorScheme.primary)),
-                          subtitle:Text(currentReward.description,textAlign: TextAlign.justify),
-                          trailing: FilledButton(
-                            style: FilledButton.styleFrom(foregroundColor: AppTheme.colorScheme.onSecondary,backgroundColor: AppTheme.colorScheme.secondary),
-                            onPressed: () => {},
-                            child:  Text("\$" + currentReward.price.toString())
-                          )
+                      return Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: ListTile(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(3),
+                                side: BorderSide(color: Colors.black)),
+                            tileColor: AppTheme.colorScheme.primaryContainer,
+                            contentPadding: EdgeInsets.all(10),
+                            leading: Image.network(currentReward.image),
+                            title: Text(currentReward.title,
+                                style: AppTheme.themeData.textTheme.titleSmall!
+                                    .copyWith(
+                                        color: AppTheme.colorScheme.primary)),
+                            subtitle: Text(currentReward.description,
+                                textAlign: TextAlign.justify),
+                            trailing: FilledButton(
+                                style: FilledButton.styleFrom(
+                                    foregroundColor:
+                                        AppTheme.colorScheme.onSecondary,
+                                    backgroundColor:
+                                        AppTheme.colorScheme.secondary),
+                                onPressed: () => {},
+                                child: Text(
+                                    "\$" + currentReward.price.toString()))),
                       );
                     }));
           } else {
@@ -69,27 +84,25 @@ class _RewardsPageState extends State<RewardsPage> {
           }
         });
   }
-Widget _userCoins(BuildContext context){
-return BlocBuilder<userBloc.UserBloc,userBloc.UserState>(
-  bloc: _userBloc,
-    builder: (context,state){
-      if(state is userBloc.Loading){
-        return  Text("Informação do usuário carregando");
-      }else if(state is userBloc.Loaded){
+
+  Widget _userCoins(BuildContext context) {
+    return BlocBuilder<userBloc.UserBloc, userBloc.UserState>(
+        bloc: _userBloc,
+        builder: (context, state) {
+          if (state is userBloc.Loading) {
+            return Text("Informação do usuário carregando");
+          } else if (state is userBloc.Loaded) {
             return Text("punched cards: " + state.user.coins.toString());
-      }else{
-        return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Text(
-              "Erro ao carregar informação do usuário.",
-              style: TextStyle(color: AppTheme.colorScheme.error),
-            ));
-      }
-    });
-
-}
-
-
+          } else {
+            return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "Erro ao carregar informação do usuário.",
+                  style: TextStyle(color: AppTheme.colorScheme.error),
+                ));
+          }
+        });
+  }
 }
 
 class _Loading extends StatelessWidget {
@@ -121,5 +134,3 @@ class _FailedLoad extends StatelessWidget {
         ));
   }
 }
-
-
