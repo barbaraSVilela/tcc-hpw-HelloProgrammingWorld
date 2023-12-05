c05af04f73f3f35bfade8a55f13fca4639a472fc
HPW <hpw.application@gmail.com>
2023-12-05 09:25:34 -0300

    fix widget test

--- test/challenge_page_test.dart
+++ test/challenge_page_test.dart
@@ -5,9 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:bloc_presentation_test/bloc_presentation_test.dart';
 import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
+import 'package:tcc_hpw_hello_programming_world/components/pill_container.dart';
 import 'package:tcc_hpw_hello_programming_world/features/challenge/domain/entity/challenge.dart';
 import 'package:tcc_hpw_hello_programming_world/features/challenge/presentation/bloc/challenge_bloc.dart';
 import 'package:tcc_hpw_hello_programming_world/features/challenge/presentation/pages/challenge_page.dart';
@@ -16,28 +19,41 @@ class MockChallengeBloc extends MockBloc<ChallengeEvent, ChallengeState>
     implements ChallengeBloc {}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Loaded challenge is displayed as expected',
+      (WidgetTester tester) async {
     final bloc = MockChallengeBloc();
     var state = const ChallengeState.loaded(
         challenge: Challenge(
           id: '',
-          prompt: '',
+          prompt: 'Challenge prompt',
           level: 0,
           coins: 0,
           solution: [],
-          options: [],
         ),
+        availableOptions: ['option 1', 'option 2'],
         attemptsLeft: 0);
     whenListen(
       bloc,
       Stream.fromIterable([state]),
       initialState: state,
     );
+    whenListenPresentation(bloc);
 
     GetIt.I.registerLazySingleton<ChallengeBloc>(() => bloc);
 
-    await tester.pumpWidget(const ChallengePage());
+    await tester
+        .pumpWidget(const MaterialApp(home: Scaffold(body: ChallengePage())));
 
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Challenge prompt'), findsOneWidget);
+
+    expect(
+        find.byWidgetPredicate(
+            (widget) => widget is PillContainer && widget.text == 'option 1'),
+        findsOneWidget);
+
+    expect(
+        find.byWidgetPredicate(
+            (widget) => widget is PillContainer && widget.text == 'option 2'),
+        findsOneWidget);
   });
 }
